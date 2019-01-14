77ff57e4ef38ff9f80acff4733094c04ff97505a
jmjm <jmmaniego@hotmail.com>
2019-01-15 01:07:11 +0800

    Updates

---
+++ precache-manifest.b51b379ecc7e8048e8a3fe59210aebb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f8b106487841bca5888",
+    "url": "/blog/static/css/main.b2ef852a.chunk.css"
+  },
+  {
+    "revision": "0f8b106487841bca5888",
+    "url": "/blog/static/js/main.0f8b1064.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/blog/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "255cd0f7ab465faad54a",
+    "url": "/blog/static/js/runtime~main.255cd0f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ba6b1dcb2890ee98a119afc424800f2",
+    "url": "/blog/index.html"
+  }
+];
