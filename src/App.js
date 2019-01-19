61686b1491be3b1d8e71a9a738a4ec5785831816
jmjm <jmmaniego@hotmail.com>
2019-01-20 00:31:20 +0800

    blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Switch, Link, Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 blog-sidebar">
+            <div className="blog-sidebar__content">
+              <h6>JM Maniego</h6>
+              <div className="mb-5">Ruby on Rails developer</div>
+              <ul className="nav justify-content-end">
+                <li className="nav-item ml-3 mb-2">
+                  <Link className="btn btn-outline-secondary" to="blog">Home</Link>
+                </li>
+                <li className="nav-item ml-3 mb-2">
+                  <Link className="btn btn-outline-secondary" to="about">About</Link>
+                </li>
+                <li className="nav-item ml-3 mb-2">
+                  <Link className="btn btn-outline-secondary" to="contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-9 blog-content">
+            <Switch>
+              <Route path="/blog" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/" exact render={() => <Redirect to="/blog" />} />
+            </Switch>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <ol className="list-group list-group-flush article-list">
+        <li className="list-group-item">
+          <article>
+            <a href="#">
+              <h1>Hello World</h1>
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              ipsum diam, consequat vitae ipsum pellentesque, varius
+              pellentesque velit. Etiam mollis ex nec cursus gravida. Ut
+              imperdiet posuere nisl, at hendrerit quam vehicula a. In et
+              faucibus nibh, ut varius ante. Nunc sodales vehicula elit in
+              fermentum. Vestibulum condimentum malesuada tortor in malesuada.
+              Curabitur ut finibus turpis, sed maximus ex. Donec viverra rutrum
+              lacus.
+            </p>
+          </article>
+        </li>
+        <li className="list-group-item">
+          <article>
+            <a href="#">
+              <h1>Hello World</h1>
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              ipsum diam, consequat vitae ipsum pellentesque, varius
+              pellentesque velit. Etiam mollis ex nec cursus gravida. Ut
+              imperdiet posuere nisl, at hendrerit quam vehicula a. In et
+              faucibus nibh, ut varius ante. Nunc sodales vehicula elit in
+              fermentum. Vestibulum condimentum malesuada tortor in malesuada.
+              Curabitur ut finibus turpis, sed maximus ex. Donec viverra rutrum
+              lacus.
+            </p>
+          </article>
+        </li>
+        <li className="list-group-item">
+          <article>
+            <a href="#">
+              <h1>Hello World</h1>
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              ipsum diam, consequat vitae ipsum pellentesque, varius
+              pellentesque velit. Etiam mollis ex nec cursus gravida. Ut
+              imperdiet posuere nisl, at hendrerit quam vehicula a. In et
+              faucibus nibh, ut varius ante. Nunc sodales vehicula elit in
+              fermentum. Vestibulum condimentum malesuada tortor in malesuada.
+              Curabitur ut finibus turpis, sed maximus ex. Donec viverra rutrum
+              lacus.
+            </p>
+          </article>
+        </li>
+      </ol>
+    );
+  }
+}
+
+export default Home;
